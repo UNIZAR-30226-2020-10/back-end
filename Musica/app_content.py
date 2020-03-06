7b2ef7d418bd727e1a1a1ba3497827bec44e8d75
saul205 <saulflobe@hotmail.com>
2020-03-06 18:05:10 +0100

    Prueba reproducir cancion

--- Musica/app_content.py
+++ Musica/app_content.py
@@ -1,7 +1,18 @@
+import flask as f
+
 from create_app import app
 from db import db
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return f.render_template('index.html')
+
+
+def generate():
+    yield 'static/musica/file_example_MP3_1MG.mp3'
+
+
+@app.route('/', methods=['POST'])
+def reproduce():
+    return f.render_template('index.html', response=generate())
