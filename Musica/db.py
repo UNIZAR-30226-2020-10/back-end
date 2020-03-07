fd23716d4e493ad16e1a3aac008ff8befd7e95b2
Alberto Calvo <albertocalvojaca@gmail.com>
2020-03-08 00:57:40 +0100

    Primera version de clases. Faltan relaciones

--- Musica/db.py
+++ Musica/db.py
@@ -12,24 +12,80 @@ app.config['SQLALCHEMY_DATABASE_URI'] = \
 
 db = SQLAlchemy(app)
 
+categorizacion = db.Table('categorizacion',
+                          db.Column('categoria', db.String(20), db.ForeignKey('categoria.nombre')),
+                          db.Column('cancion', db.String(20), db.ForeignKey('cancion.id'))
+                          )
+
+
+class Categoria(db.Model):
+    nombre = db.Column(db.String(20), primary_key=True)
+    descripcion = db.Column(db.String(100))
+    canciones = db.relationship('Cancion', secondary=categorizacion, backref=db.backref('categorias', lazy='dynamic'))
+
+
+class Artista(db.Model):
+    nombre = db.Column(db.String(20), primary_key=True)
+    fecha_nacimiento = db.Column(db.Date)
+    pais = db.Column(db.String(40))
+    alias = db.Column(db.String(20))
+
+
+class Album(db.Model):
+    nombre = db.Column(db.String(20), primary_key=True)
+    descripcion = db.Column(db.String(100))
+    fecha = db.Column(db.Date)
+
+
+class Lista(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(20), nullable=False)
+    descripcion = db.Column(db.String(100))
+
+
+class Usuario(db.Model):
+    email = db.Column(db.String(25), primary_key=True)
+    nombre = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String, nullable=False)
+    fecha_nacimiento = db.Column(db.Date)
+    pais = db.Column(db.String(40))
+
+
+class Solicitud(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+
+class ListaCompartida(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+
+class PistaCompartida(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
 
 class Cancion(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    path = db.Column(db.String(100), unique=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)  # Cambiar por clave compuesta
+    audio = db.Column(db.LargeBinary, nullable=False)  # Comprobar como funciona
+    nombre = db.Column(db.String(20), nullable=False)
+    duracion = db.Column(db.Integer, nullable=False)  # Segundos
 
-    def __init__(self, name):
-        self.path = name
+
+# class Cancion(db.Model):
+#     path = db.Column(db.String(100), unique=True, nullable=False)
+#
+#     def __init__(self, name):
+#         self.path = name
 
 
 db.create_all()
 
 
-def add_song(song):
-    if not Cancion.query.filter_by(path=song.path):
-
-        db.session.add(song)
-        db.session.commit()
-        return False
-
-    else:
-        return True
+# def add_song(song):
+#     if not Cancion.query.filter_by(path=song.path):
+#
+#         db.session.add(song)
+#         db.session.commit()
+#         return False
+#
+#     else:
+#         return True
