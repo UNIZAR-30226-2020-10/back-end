10b0070ec874b19c6179d46121d6b26c2a3ec9bf
Alberto Calvo <albertocalvojaca@gmail.com>
2020-03-12 00:49:22 +0100

    Clases terminadas y db_test.py con pruebas de las clases y relaciones

--- Musica/db.py
+++ Musica/db.py
@@ -27,41 +27,44 @@ composicion = db.Table('composicion',
                        db.Column('cancion', db.Integer, db.ForeignKey('cancion.id'))
                        )
 # Relacion 'publica'
-publicacion = db.table('publicacion',
+publicacion = db.Table('publicacion',
                        db.Column('artista', db.String(20), db.ForeignKey('artista.nombre')),
                        db.Column('album', db.String(20), db.ForeignKey('album.nombre'))
                        )
-# Relacion aparece
-aparicion = db.table('aparicion',
+
+# Relacion 'aparece'
+aparicion = db.Table('aparicion',
                      db.Column('lista', db.Integer, db.ForeignKey('lista.id')),
                      db.Column('cancion', db.Integer, db.ForeignKey('cancion.id'))
                      )
+
 # Relacion 'conoce'
-amistad = db.table('amistad',
-                   db.Column('usuario', db.String(25), db.ForeignKey('usuario.email')),
-                   db.Column('usuario', db.String(25), db.ForeignKey('usuario.email'))
+amistad = db.Table('amistad',
+                   db.Column('usuario1', db.String(25), db.ForeignKey('usuario.email')),
+                   db.Column('usuario2', db.String(25), db.ForeignKey('usuario.email'))
                    )
 
 
 class Categoria(db.Model):
     nombre = db.Column(db.String(20), primary_key=True)
     descripcion = db.Column(db.String(100))
-    canciones = db.relationship('Cancion', secondary=categorizacion, backref=db.backref('categorias', lazy='dynamic'))
+    canciones = db.relationship('Cancion', secondary=categorizacion, backref=db.backref('categorias'))
 
 
 class Artista(db.Model):
     nombre = db.Column(db.String(20), primary_key=True)
-    fecha_nacimiento = db.Column(db.Date)
+    fecha_nacimiento = db.Column(db.DateTime)
     pais = db.Column(db.String(40))
     alias = db.Column(db.String(20))
-    composiciones = db.relationship('Cancion', secondary=composicion, backref=db.backref('artistas', lazy='dynamic'))
-    publicaciones = db.relationship('Album', secondary=publicacion, backref=db.backref('artistas', lazy='dynamic'))
+    composiciones = db.relationship('Cancion', secondary=composicion, backref=db.backref('artistas'))
+    publicaciones = db.relationship('Album', secondary=publicacion,
+                                    backref=db.backref('artistas'))
 
 
 class Album(db.Model):
     nombre = db.Column(db.String(20), primary_key=True)
     descripcion = db.Column(db.String(100))
-    fecha = db.Column(db.Date)
+    fecha = db.Column(db.DateTime)
     canciones = db.relationship('Cancion', backref='album')  # Relacion 'compuesto'
 
 
@@ -69,7 +72,7 @@ class Lista(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(20), nullable=False)
     descripcion = db.Column(db.String(100))
-    canciones = db.relationship('Cancion', secondary=aparicion, backref=db.backref('listas', lazy='dynamic'))
+    canciones = db.relationship('Cancion', secondary=aparicion, backref=db.backref('listas'))
     email_usuario = db.Column(db.String(25), db.ForeignKey('usuario.email'))
     comparticiones = db.relationship('ListaCompartida', backref='lista')  # Relacion 'compartida'
 
@@ -78,18 +81,25 @@ class Usuario(db.Model):
     email = db.Column(db.String(25), primary_key=True)
     nombre = db.Column(db.String(20), nullable=False)
     password = db.Column(db.String, nullable=False)
-    fecha_nacimiento = db.Column(db.Date)
+    fecha_nacimiento = db.Column(db.DateTime)
     pais = db.Column(db.String(40))
+    amistades = db.relationship('Usuario', secondary='amistad', primaryjoin=email == amistad.c.usuario1,
+                                secondaryjoin=amistad.c.usuario2 == email)
+    listas = db.relationship('Lista', backref='usuario')  # Relacion 'tiene'
     id_ultima_cancion = db.Column(db.Integer, db.ForeignKey('cancion.id'))
     segundo_ultima_cancion = db.Column(db.Integer)
-    listas = db.relationship('Lista', backref='usuario')  # Relacion 'tiene'
-    amistades = db.relationship('Usuario', secondary='amistades')
-    solicitudes_recibidas = db.relationship('Solicitud', backref='notificado')  # Relacion 'recibe'
-    canciones_recibidas = db.relationship('CancionCompartida', backref='notificado')  # Relacion 'recibe'
-    listas_recibidas = db.relationship('ListaCompartida', backref='notificado')  # Relacion 'recibe'
-    solicitudes_enviadas = db.relationship('Solicitud', backref='notificante')  # Relacion 'envia'
-    canciones_enviadas = db.relationship('CancionCompartida', backref='notificante')  # Relacion 'envia'
-    listas_enviadas = db.relationship('ListaCompartida', backref='notificante')  # Relacion 'envia'
+    solicitudes_recibidas = db.relationship('Solicitud', backref='notificado',
+                                            foreign_keys="Solicitud.email_usuario_notificado")  # Relacion 'recibe'
+    solicitudes_enviadas = db.relationship('Solicitud', backref='notificante',
+                                           foreign_keys="Solicitud.email_usuario_notificante")  # Relacion 'envia'
+    listas_recibidas = db.relationship('ListaCompartida', backref='notificado',
+                                       foreign_keys="ListaCompartida.email_usuario_notificado")  # Relacion 'recibe'
+    listas_enviadas = db.relationship('ListaCompartida', backref='notificante',
+                                      foreign_keys="ListaCompartida.email_usuario_notificante")  # Relacion 'envia'
+    canciones_enviadas = db.relationship('CancionCompartida', backref='notificante',
+                                         foreign_keys="CancionCompartida.email_usuario_notificante")  # Relacion 'envia'
+    canciones_recibidas = db.relationship('CancionCompartida', backref='notificado',
+                                          foreign_keys="CancionCompartida.email_usuario_notificado")  # Relacion 'recibe'
 
 
 class Solicitud(db.Model):
@@ -114,29 +124,9 @@ class CancionCompartida(db.Model):
 
 class Cancion(db.Model):
     id = db.Column(db.Integer, primary_key=True)  # Cambiar por clave compuesta
-    audio = db.Column(db.LargeBinary, nullable=False)  # Comprobar como funciona
+    path = db.Column(db.String(50), nullable=False)
     nombre = db.Column(db.String(20), nullable=False)
     duracion = db.Column(db.Integer, nullable=False)  # Segundos
     nombre_album = db.Column(db.String(20), db.ForeignKey('album.nombre'))
     comparticiones = db.relationship('CancionCompartida', backref='cancion')  # Relacion 'compartida'
     reproducciones = db.relationship('Usuario', backref='ultima_cancion')  # Relacion 'ultima'
-
-
-# class Cancion(db.Model):
-#     path = db.Column(db.String(100), unique=True, nullable=False)
-#
-#     def __init__(self, name):
-#         self.path = name
-
-
-db.create_all()
-
-# def add_song(song):
-#     if not Cancion.query.filter_by(path=song.path):
-#
-#         db.session.add(song)
-#         db.session.commit()
-#         return False
-#
-#     else:
-#         return True
---
+++ Musica/db_test.py
@@ -0,0 +1,133 @@
+from app import app
+from db import *
+import datetime
+
+
+def poblar():
+    categorias = [
+        Categoria(nombre='Rock', descripcion='Categoria Rock'),
+        Categoria(nombre='Pop', descripcion='Categoria Pop')
+    ]
+
+    artistas = [
+        Artista(nombre='Juan', fecha_nacimiento=datetime.datetime(1990, 1, 2)),
+        Artista(nombre='Paco', fecha_nacimiento=datetime.datetime(1991, 5, 1))
+    ]
+
+    albumes = [
+        Album(nombre='Mi album 1', descripcion='album 1', fecha=datetime.datetime(2000, 6, 1)),
+        Album(nombre='Mi album 2', descripcion='album 2', fecha=datetime.datetime(2015, 6, 1))
+    ]
+
+    listas = [
+        Lista(nombre='Favoritas', descripcion='Canciones favoritas'),
+        Lista(nombre='Gimnasio', descripcion='Canciones para entrenar en el gimnasio')
+    ]
+
+    usuarios = [
+        Usuario(email='paco@gmail.com', nombre='Paco', password='passpaco',
+                fecha_nacimiento=datetime.datetime(2000, 1, 1), pais='Spain'),
+        Usuario(email='laura@gmail.com', nombre='Laura', password='passlaura',
+                fecha_nacimiento=datetime.datetime(2001, 1, 1), pais='France')
+    ]
+
+    solicitudes = [
+        Solicitud(),
+        Solicitud()
+    ]
+
+    listas_compartidas = [
+        ListaCompartida(),
+        ListaCompartida()
+    ]
+
+    canciones_compartidas = [
+        CancionCompartida(),
+        CancionCompartida()
+    ]
+
+    canciones = [
+        Cancion(path='/home/alberto/canciones', nombre='Hello world', duracion=120),
+        Cancion(path='/home/juan/canciones', nombre='Foo', duracion=180)
+    ]
+
+    db.session.add_all(categorias)
+    db.session.add_all(artistas)
+    db.session.add_all(albumes)
+    db.session.add_all(listas)
+    db.session.add_all(usuarios)
+    db.session.add_all(solicitudes)
+    db.session.add_all(listas_compartidas)
+    db.session.add_all(canciones_compartidas)
+    db.session.add_all(canciones)
+
+    # Añadir relaciones
+
+    # Relacion comprende
+    categorias[0].canciones.append(canciones[0])
+    categorias[0].canciones.append(canciones[1])
+    canciones[1].categorias.append(categorias[1])
+
+    # Relacion composicion
+    artistas[0].composiciones.append(canciones[0])
+    artistas[0].composiciones.append(canciones[1])
+    canciones[1].artistas.append(artistas[1])
+
+    # Relacion publicacion
+    artistas[0].publicaciones.append(albumes[0])
+    artistas[0].publicaciones.append(albumes[1])
+    albumes[0].artistas.append(artistas[1])
+
+    # Relacion aparece
+    listas[0].canciones.append(canciones[0])
+    listas[0].canciones.append(canciones[1])
+    canciones[1].listas.append(listas[1])
+
+    # Relacion amistad
+    usuarios[0].amistades.append(usuarios[1])
+    usuarios[1].amistades.append(usuarios[0])
+
+    # Relaciones 1:N
+
+    # Relacion compuesto
+    albumes[0].canciones.append(canciones[0])
+    albumes[0].canciones.append(canciones[1])
+    canciones[1].album = albumes[1]  # Sobreescribe lo anterior aposta
+
+    # Relacion tiene
+    usuarios[0].listas.append(listas[0])
+    usuarios[1].listas.append(listas[1])
+    listas[0].email_usuario = usuarios[0].email
+
+    # Relacion compartida (lista)
+    listas[1].comparticiones.append(listas_compartidas[0])
+    listas[1].comparticiones.append(listas_compartidas[1])
+
+    # Relacion compartida (cancion)
+    canciones[0].comparticiones.append(canciones_compartidas[0])
+    canciones[0].comparticiones.append(canciones_compartidas[1])
+
+    # Relacion ultima
+    usuarios[0].id_ultima_cancion = canciones[0].id
+    usuarios[1].id_ultima_cancion = canciones[0].id
+
+    # Relaciones recibe y envia (solicitud, listacompartida y cancioncompartida)
+    solicitudes = [
+        Solicitud(email_usuario_notificado=usuarios[0].email, email_usuario_notificante=usuarios[1].email),
+        Solicitud(email_usuario_notificado=usuarios[1].email, email_usuario_notificante=usuarios[0].email)
+    ]
+    db.session.add_all(solicitudes)
+
+    listas_compartidas = [
+        ListaCompartida(email_usuario_notificado=usuarios[0].email, email_usuario_notificante=usuarios[1].email),
+        ListaCompartida(email_usuario_notificado=usuarios[1].email, email_usuario_notificante=usuarios[0].email)
+    ]
+    db.session.add_all(listas_compartidas)
+
+    canciones_compartidas = [
+        CancionCompartida(email_usuario_notificado=usuarios[0].email, email_usuario_notificante=usuarios[1].email),
+        CancionCompartida(email_usuario_notificado=usuarios[1].email, email_usuario_notificante=usuarios[0].email)
+    ]
+    db.session.add_all(canciones_compartidas)
+
+    db.session.commit()
