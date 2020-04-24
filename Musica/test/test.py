7daeec884ec6cd93f5d1b2cffaf959d3d5b8bf4e
saul205 <saulflobe@hotmail.com>
2020-04-24 21:11:55 +0200

    Test no duplicados

--- Musica/test/test.py
+++ Musica/test/test.py
@@ -5,7 +5,7 @@ DB.drop_all()
 DB.create_all()
 
 loader = unittest.TestLoader()
-tests = loader.discover(".")
+tests = loader.discover('.', "test_server.py")
 testRunner = unittest.runner.TextTestRunner()
 testRunner.run(tests)
 
